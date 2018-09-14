c5a176dfc2aae4fa4685524410d361cebeb5c0d0
lena <rublen77@gmail.com>
2018-09-14 23:25:56 +0300

    questions_controller + tests

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,5 +1,128 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  describe 'GET #index' do
+    let(:questions) { create_list(:question, 2) }
+    before { get :index }
 
+    it 'populates an array of all questions' do
+      expect(assigns(:questions)).to match_array(questions)
+    end
+
+    it 'renders index view' do
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET #show' do
+    let(:question) { create(:question) }
+    before { get :show, params: { id: question } }
+
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'assigns a new question to @question' do
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+    let(:question) { create(:question) }
+    before { get :edit, params: { id: question } }
+
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders edit view' do
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valide attributes' do
+      it 'saves a new question in the DB' do
+        expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).by(1)
+      end
+
+      it 'redirects to show view' do
+        post :create, params: { question: attributes_for(:question) }
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+
+    context 'with invalide attributes' do
+      it 'does not save the question in the DB' do
+        expect { post :create, params: { question: attributes_for(:invalid_question) } }.to_not change(Question, :count)
+      end
+
+      it 'renders new view' do
+        post :create, params: { question: attributes_for(:invalid_question) }
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    let(:question) { create(:question) }
+
+    context 'with valide attributes' do
+      it 'assigns the requested question to @question' do
+        patch :update, params: { id: question, question: attributes_for(:question) }
+        expect(assigns(:question)).to eq question
+      end
+
+      it 'changes question attributes' do
+        patch :update, params: { id: question, question: { title: 'new tit', body: 'new b'} }
+        question.reload
+        expect(question.title).to eq 'new tit'
+        expect(question.body).to eq 'new b'
+      end
+
+      it 'redirects to the updated question' do
+        patch :update, params: { id: question, question: { title: 'new tit', body: 'new b'} }
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+
+    context 'with invalide attributes' do
+      before { patch :update, params: { id: question, question: attributes_for(:invalid_question) } }
+
+      it 'does not change question attributes' do
+        question.reload
+        expect(question.title).to eq 'MyString'
+        expect(question.body).to eq 'MyText'
+      end
+
+      it 'renders edit view' do
+        expect(response).to render_template :edit
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let!(:question) { create(:question) }
+
+    it 'deletes question' do
+      expect { delete :destroy, params: { id: question } }.to change(Question, :count).by(-1)
+    end
+
+    it 'redirects to index view' do
+      delete :destroy, params: { id: question }
+      expect(response).to redirect_to questions_path
+    end
+  end
 end
