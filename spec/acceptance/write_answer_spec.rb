44b43cb1fabe5ea2ed26efc8c4986a01c572fd8e
lena <rublen77@gmail.com>
2018-09-17 13:15:40 +0300

    acc-test: questions list

---
+++ spec/acceptance/write_answer_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'Write Answer', %q{
+  In order to answer the current question
+  As a user
+  I want to be able to write my answer on the current question's page
+} do
+
+  given(:question) { create(:question) }
+  given!(:answer) { create(:answer, question: question) }
+
+  scenario "User can write the answer on the question's page" do
+    visit question_path(question)
+
+    click_on 'Answer the question'
+    fill_in 'Body', with: answer.body
+    click_on 'Publish'
+
+    expect(page).to have_content answer.body
+  end
+
+end
