e1cb87d744605c7b5720aa484c7fe8bdbf7c6327
lena <rublen77@gmail.com>
2018-10-04 12:52:55 +0300

    ajax2: acc.testing of best answer

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -47,20 +47,6 @@ RSpec.describe QuestionsController, type: :controller do
     end
   end
 
-  # describe 'GET #edit' do
-  #   sign_in_user
-  #   let(:question) { create(:question, author: @user) }
-  #   before { get :edit, params: { id: question } }
-
-  #   it 'assigns the requested question to @question' do
-  #     expect(assigns(:question)).to eq question
-  #   end
-
-  #   it 'renders edit view' do
-  #     expect(response).to render_template :edit
-  #   end
-  # end
-
   describe 'POST #create' do
     sign_in_user
     context 'with valide attributes' do
