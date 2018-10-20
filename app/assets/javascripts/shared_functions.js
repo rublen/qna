48bb2ea321941a1deb7aba951be5469d610c2b81
lena <rublen77@gmail.com>
2018-10-20 18:55:25 +0300

    bootstrap

--- app/assets/javascripts/shared_functions.js
+++ app/assets/javascripts/shared_functions.js
@@ -1,16 +1,19 @@
 function editHideClassHandler(model, id) {
+  cleanFlash()
   document.querySelector('[data-id="' + id + '"]', '[data-model="' + model + '"]').classList.toggle('hide');
   document.querySelector('#edit-' + model + '-form-' + id).classList.toggle('hide');
 };
 
 function cleanFlash() {
-  document.querySelector('.alert').remove();
+  var alert = document.querySelector('.alert');
+  if (alert) { alert.remove() };
 };
 
 function showFlash(innerHtml) {
+  cleanFlash();
   var flash = document.createElement('div');
   var nodeAfterFlash = document.querySelector('.navbar').nextSibling;
 
-  flash.innerHTML = innerHtml;
+  flash.innerHTML = innerHtml.trim();
   document.body.insertBefore(flash, nodeAfterFlash);
 };
