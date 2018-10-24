0f9df431d68571edfc0918e066bd3d29f3fe8811
lena <rublen77@gmail.com>
2018-10-24 09:28:13 +0300

    answers voting: frontend

--- app/assets/javascripts/vote.js
+++ app/assets/javascripts/vote.js
@@ -1,41 +1,51 @@
 document.addEventListener('turbolinks:load', function() {
-  var voteUp = document.querySelector('.vote-up');
-  var voteDown = document.querySelector('.vote-down');
-  var sum = voteUp.parentElement.querySelector('.vote-sum');
+  var voteUps = document.querySelectorAll('.vote-up');
+  var voteDowns = document.querySelectorAll('.vote-down');
 
+  var voteUp = document.querySelector('.vote-up');
   var defaultColor = window.getComputedStyle(voteUp).color;
   var voteUpHoverColor = "lightgreen";
   var voteDownHoverColor = "red";
 
   function hoverColor(elem) {
-    return (elem == voteUp ? voteUpHoverColor : voteDownHoverColor)
-  }
+    var voteUpsArr = Array.prototype.slice.call(voteUps);
+    return (voteUpsArr.includes(elem) ? voteUpHoverColor : voteDownHoverColor)
+  };
 
-  voteUp.addEventListener('click', function() {
-    var voted = sum.dataset.voted;
-    var upVoted = voteUp.dataset.voted;
+  voteUps.forEach(function(voteUp) {
+    voteUp.addEventListener('click', function() {
+      var sum = voteUp.parentElement.querySelector('.vote-sum');
+      var voteDown = voteUp.parentElement.querySelector('.vote-down');
+      var voted = sum.dataset.voted;
+      var upVoted = voteUp.dataset.voted;
 
-    if (voted == '1' && upVoted == '1') {
-      unvoting(voteUp, voteDown, 1);
-    } else { if (voted == '0') {
-      voting(voteUp, voteDown, 1)
+      if (voted == '1' && upVoted == '1') {
+        unvoting(voteUp, voteDown, 1);
+      } else { if (voted == '0') {
+        voting(voteUp, voteDown, 1)
+        }
       }
-    }
+    })
   });
 
-  voteDown.addEventListener('click', function() {
-    var voted = sum.dataset.voted;
-    var downVoted = voteDown.dataset.voted;
+  voteDowns.forEach(function(voteDown) {
+    voteDown.addEventListener('click', function() {
+      var sum = voteDown.parentElement.querySelector('.vote-sum');
+      var voteUp = voteDown.parentElement.querySelector('.vote-up');
+      var voted = sum.dataset.voted;
+      var downVoted = voteDown.dataset.voted;
 
-    if (voted == '1' && downVoted =='1') {
-      unvoting(voteDown, voteUp, -1);
-    } else { if (voted == '0') {
-      voting(voteDown, voteUp, -1)
+      if (voted == '1' && downVoted =='1') {
+        unvoting(voteDown, voteUp, -1);
+      } else { if (voted == '0') {
+        voting(voteDown, voteUp, -1)
+        }
       }
-    }
+    })
   });
 
   function voting(elem, other, diff) {
+    var sum = elem.parentElement.querySelector('.vote-sum');
     var sumValue = parseInt(sum.textContent) + diff;
     sum.textContent = sumValue;
     elem.dataset.voted = 1;
@@ -47,6 +57,7 @@ document.addEventListener('turbolinks:load', function() {
   };
 
   function unvoting(elem, other, diff) {
+    var sum = elem.parentElement.querySelector('.vote-sum');
     var sumValue = parseInt(sum.textContent) - diff;
     sum.textContent = sumValue;
     elem.dataset.voted = 0;
