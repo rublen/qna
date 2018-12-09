5cf70c5945c30ed2984eda98dc965e83c395a4bb
lena <rublen77@gmail.com>
2018-12-09 03:06:37 +0200

    rest_api: api/v1/profiles/me, tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,7 +41,6 @@ RSpec.configure do |config|
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.extend ControllerMacros, type: :controller
   config.extend ControllerMacros, type: :request
-  # config.include RequestSpecHelper, type: :request - ???????????
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
