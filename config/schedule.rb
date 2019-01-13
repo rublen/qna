ef93fc708801a12db666f889cf926d7351084cf2
lena <rublen77@gmail.com>
2019-01-13 23:03:26 +0200

    deploy: set Sphinx index schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,12 +7,10 @@
 #
 # set :output, "/path/to/my/cron_log.log"
 #
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
+every 60.minutes do
+  rake "ts:index"
+end
+
 every 1.days do
   runner "DailyMailerJob.perform_later"
 end
