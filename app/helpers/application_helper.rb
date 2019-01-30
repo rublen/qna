a43f3d6e4d2ebc0dbdbf1b6c5cb1ff5b6010f13a
lena <rublen77@gmail.com>
2019-01-30 17:35:57 +0200

    correct cash keys

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,3 @@
 module ApplicationHelper
   include Pagy::Frontend
-
-  def current_user_role(obj)
-    return "guest" unless current_user
-    return "admin" if current_user.admin?
-    current_user.author_of?(obj) ? "author" : "other"
-  end
 end
