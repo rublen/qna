bf889f2a500a859d00ff692a37c6dd433c2a8e88
lena <rublen77@gmail.com>
2019-01-31 10:41:56 +0200

    helper, corrections

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def current_user_role(obj)
+    return "guest" unless current_user
+    return "author" if (current_user.author_of?(obj) && !current_user.admin?)
+
+    role = current_user.admin? ? "admin" : "other"
+
+    return role + "/not_voted" unless obj.voted?(current_user)
+    role + (obj.upvoted?(current_user) ? "/upvoted" : "/downvoted")
+  end
 end
